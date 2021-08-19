9c2ba3c21c5e4f2658e4189967d3480fcb0ddb54
Jibrangit <jibran.university@gmail.com>
2021-08-19 17:18:23 -0400

    Added depth-traversal methods to the tree

--- src/challenges/treeTraversal.cpp
+++ src/challenges/treeTraversal.cpp
@@ -16,6 +16,9 @@ public:
     }
     void createTree();
     Node* getRoot() {return rootNode;}
+    void preOrderTraversal(Node *);
+    void postOrderTraversal(Node *);
+    void inOrderTraversal(Node *);
     ~Tree() {delete rootNode; }
     
 private:
@@ -63,12 +66,53 @@ void Tree::createTree() {
         
     }
 }
+
+void Tree::preOrderTraversal(Node *node)
+{
+    if(node == nullptr)                              // When the last node (leaf) is hit, this ends that call and it gets popped off the stack and goes back to previous node. 
+        return;
+    
+    cout << node->letter << endl;
+
+    preOrderTraversal(node->left);                    // Keeps going to left sub-trees, ditto for the right. 
+
+    preOrderTraversal(node->right);
+
+}
+
+void Tree::postOrderTraversal(Node *node)
+{
+    if(node == nullptr)
+        return;
+    
+    postOrderTraversal(node->left);
+
+    postOrderTraversal(node->right);
+    
+    cout << node->letter << endl;
+}
+
+void Tree::inOrderTraversal(Node *node)
+{
+    if(node == nullptr)
+        return;
+    
+    inOrderTraversal(node->left);
+
+    cout << node->letter << endl;
+
+    inOrderTraversal(node->right);
+}
 int main(int argc, char const *argv[])
 {
     std::vector<char> letters= {'L', 'C', 'I', 'H', 'T', 'X', 'J', 'R', '\0'};
     Tree tree(letters);
     tree.createTree();
-    cout << tree.getRoot()->right->right->letter << endl; // Testing the tree
+    // cout << tree.getRoot()->right->right->letter << endl; // Testing the tree
+
+    // tree.preOrderTraversal(tree.getRoot());
+    // tree.postOrderTraversal(tree.getRoot());
+    tree.inOrderTraversal(tree.getRoot());
 
 
     return 0;
