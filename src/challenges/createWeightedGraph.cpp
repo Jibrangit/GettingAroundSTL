97188c324fc53d663a59b44492093abe9d1d6c0e
Jibrangit <jibran.university@gmail.com>
2021-08-21 18:10:40 -0400

    Made graph with vertices, yet to figure out how to make edges

--- src/challenges/createWeightedGraph.cpp
+++ src/challenges/createWeightedGraph.cpp
@@ -1,46 +1,77 @@
 #include <iostream>
-#include <vector>
-#include <unordered_map>
+#include <unordered_set>
 #include <set>
+#include <map>
+#include <vector>
+
 
 class Vertex {
 public:
-    Vertex(char c) : ID (c) {}
-    bool operator==(const Vertex &rhs) const {    // Comparing 2 vertices, to help prevent duplication while creating the graph. 
-    return (this->ID == rhs.ID);
+    friend std::ostream &operator<<(std::ostream &os, const Vertex &v);
+    Vertex(char c) : ID_(c) {}
+    bool operator==(const Vertex &rhs) const {
+        return (this->ID_ == rhs.ID_);
     }
-    friend void Graph::insertNeighbors(std::unordered_map <Vertex*, int> neighbors);
-private:
-    char ID;
-    std::unordered_map <Vertex*, int> neighbors;   // Store pointer to the neighbor and weight of the edge connecting to it 
-                                                   // Pointer to neighbor is the 'key' here and the distance to neighbor is the 'value' in this map. 
+    bool operator<(const Vertex &rhs) const {
+        return (this->ID_ < rhs.ID_);
+    }
+
+    char ID_;
+    std::vector<std::pair<Vertex, int>> neighbors;
+    void insertEdge(char c, int weight);
 };
 
 class Graph {
 public:
+    friend std::ostream &operator<<(std::ostream &os, const Graph &g);
+    Graph(); 
     Graph(char c[]);
-    void insertNeighbors(std::unordered_map <Vertex*, int> neighbors);
+    void insertVertex(char c);
+    void insertEdge(char ID1, char ID2, int weight);
 
-private:
-    std::set <Vertex> vertices;                   // Store every unique vertex here, edge information will be stored in the vertices. 
-                                                  // Implemented as set to prevent duplicate vertices getting in. 
+protected:
+    std::set<Vertex> vertices_;
 };
 
-Graph::Graph(char c[]) {                          // When graph is intiialized with char array, insert vertices into the graph using those characters. 
-    int i{};
-    while(c[i] != '\0')
-    {
-        vertices.emplace(c[i]);
-        ++i;
+Graph::Graph(char c[]) {
+    int index{};
+    while(c[index] != '\0') {
+        vertices_.insert(c[index]);
+        ++index;
     }
 }
 
-void Graph::insertNeighbors(std::unordered_map <Vertex*, int> neighbors) {
-
-
+void Graph::insertVertex(char c) {
+    vertices_.insert(c);
 }
+
+void Graph::insertEdge(char c1, char c2, int w) {
+    // auto it1 = vertices_.find(c1);
+    // auto it2 = vertices_.find(c2);
+    // auto p1 = std::make_pair(*it1, w);
+
+    // it1->neighbors.pop_back();
+}
+
+std::ostream &operator<<(std::ostream &os, const Vertex &v) {
+    os << v.ID_;
+    return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const Graph &g) {
+    for(const auto &elem : g.vertices_) {
+        os << elem << " ";
+    }
+    return os;
+}
+
 int main(int argc, char const *argv[])
 {
-    
+    char letters[] {'A', 'C', 'D', 'E', 'B', '\0'};
+    Graph graph(letters);
+    std::cout << graph << std::endl;
+
     return 0;
 }
+
+
