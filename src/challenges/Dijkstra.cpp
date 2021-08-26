6dada0f2da0fe5e70de482031c9e0c52a792461b
Jibrangit <jibran.university@gmail.com>
2021-08-26 02:10:55 -0400

    Fixed filenames to be intuitive and divided the graph problems into number fof files

---
+++ src/challenges/Dijkstra.cpp
@@ -0,0 +1,23 @@
+#include "createWeightedGraph.h"
+
+int main(int argc, char const *argv[])
+{
+    char letters[] {'A', 'C', 'D', 'E', 'B', 'F', 'I', 'H', 'G', '\0'};
+    Graph graph(letters);
+    // std::cout << graph << std::endl;
+    graph.insertEdge('A', 'B', 9);
+    graph.insertEdge('A', 'C', 5);
+    graph.insertEdge('A', 'D', 2);
+    graph.insertEdge('B', 'E', 6);
+    graph.insertEdge('B', 'C', 1);
+    graph.insertEdge('C', 'D', 2);
+    graph.insertEdge('C', 'F', 7);
+    graph.insertEdge('C', 'G', 4);
+    graph.insertEdge('D', 'G', 5);
+    graph.insertEdge('F', 'I', 1);
+    graph.insertEdge('G', 'H', 3);
+
+    graph.Dijkstra();
+
+    return 0;
+}
--- src/challenges/robotActionSequence.cpp
+++ src/challenges/robotActionSequence.cpp
@@ -3,7 +3,9 @@
 
 /** @brief :  Given a string of combinations of G = Go, L = Left, R = Right ; determine if that sequence of actions will result in the robot moving in a circle forever. 
  * */
-bool Solution(const std::vector<std::string> &v);
+std::vector<bool> Solution(const std::vector<std::string> &v) {
+    
+}
 
 int main(int argc, char const *argv[])
 {
